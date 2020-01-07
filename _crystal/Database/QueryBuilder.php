15336d4aa2ff163de66af66f819f9792b48b77b7
parsa mpsh <parsa84sh1384@gmail.com>
2020-01-07 21:53:18 +0330

    add some functions to query builder

--- _crystal/Database/QueryBuilder.php
+++ _crystal/Database/QueryBuilder.php
@@ -19,7 +19,8 @@ class QueryBuilder{
 
 
 
-	public function orderBy($column , $type='asc'){
+	public function orderBy($column , $type='ASC'){
+		$type = strtoupper($type);
 		$this->orderBy = 'ORDER BY ' . $column . ' ' . $type;
 
 		return $this;
@@ -126,5 +127,72 @@ class QueryBuilder{
 		$sql = 'UPDATE ' . $model::tbl_name() . ' SET ' . $columns_str . ' WHERE ' . $wheres_str;
 		
 		return $model::query($sql , false);
+	}
+
+	public function count(){
+		$model = $this->model;
+
+		$wheres_str = '';
+		if($this->wheres == []){
+			$wheres_str = '1';
+		}else{
+			foreach($this->wheres as $w){
+				$wheres_str .= $w . ' ';
+			}
+		}
+
+		$sql = 'SELECT count(id) as c FROM ' . $model::tbl_name() . ' WHERE ' . $wheres_str . ' ' . $this->orderBy . $this->limit;
+		return $model::query($sql , true)[0]->c;
+	}
+
+	public function first($columns=['*']){
+		$model = $this->model;
+		if(is_string($columns)){
+			$columns = [$columns];
+		}
+
+		$wheres_str = '';
+		if($this->wheres == []){
+			$wheres_str = '1';
+		}else{
+			foreach($this->wheres as $w){
+				$wheres_str .= $w . ' ';
+			}
+		}
+
+		$sql = 'SELECT ' . Str::make_string_from_array($columns) . ' FROM ' . $model::tbl_name() . ' WHERE ' . $wheres_str . ' ' . $this->orderBy . ' LIMIT 1';
+		
+		return $model::query($sql , true)->first();
+	}
+
+	public function last($columns=['*']){
+		$model = $this->model;
+		if(is_string($columns)){
+			$columns = [$columns];
+		}
+
+		$wheres_str = '';
+		if($this->wheres == []){
+			$wheres_str = '1';
+		}else{
+			foreach($this->wheres as $w){
+				$wheres_str .= $w . ' ';
+			}
+		}
+
+		$order_by = $this->orderBy;
+		if($order_by == null){
+			$order_by = 'ORDER BY id ASC';
+		}
+		if(strpos($order_by, 'ASC')){
+			$order_by = str_replace('ASC', 'DESC', $order_by);
+		}else if(strpos($order_by, 'DESC')){
+			$order_by = str_replace('DESC', 'ASC', $order_by);
+		}
+
+
+		$sql = 'SELECT ' . Str::make_string_from_array($columns) . ' FROM ' . $model::tbl_name() . ' WHERE ' . $wheres_str . ' ' . $order_by . ' LIMIT 1';
+
+		return $model::query($sql , true)->first();
 	}
 }
