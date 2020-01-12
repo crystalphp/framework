b529258965e185b9628238bcef5dbbb4813fb406
parsa mpsh <parsa84sh1384@gmail.com>
2020-01-12 14:02:36 +0330

    complete error handler(and fix some bugs)

--- _crystal/Exception/include.php
+++ _crystal/Exception/include.php
@@ -1,14 +1,24 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('error_reporting', '1');
+
 function crystal_error_handler($errno , $errstr , $file , $line){
 	echo make_exception_render('Error' , $errstr , $file , $line - 1);
 	die();
 }
 
+function crystal_shutdown_handler(){
+	$le = error_get_last();
+	if($le !== null){
+		crystal_error_handler($le['type'] , $le['message'] , $le['file'] , $le['line']);
+	}
+}
+
+register_shutdown_function('crystal_shutdown_handler');
+
 $ex_s = glob(libs('/Exception/Exceptions/*.php'));
 include_once libs('/Exception/BaseException.php');
 include_once libs('/Exception/render.php');
+
+
 
 set_error_handler('crystal_error_handler' , E_ALL);
 
