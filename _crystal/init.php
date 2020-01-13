e03d08f6d35f4803dc80097aad5b86060cfc7bb7
parsa mpsh <parsa84sh1384@gmail.com>
2020-01-13 19:02:29 +0330

    change in start command and add .htaccess file and some changes in file init.php

--- _crystal/init.php
+++ _crystal/init.php
@@ -1,5 +1,14 @@
 <?php
 
+$path = $_SERVER['DOCUMENT_ROOT'] . $_SERVER['REQUEST_URI'];
+if(is_file($path)){
+	$type = mime_content_type($path);
+	header('Content-type: ' . $type);
+	readfile($path);
+	die();
+}
+unset($path);
+
 if( ! function_exists('app_path')){
 	function app_path($path=''){
 	    return APP_PATH . $path;
