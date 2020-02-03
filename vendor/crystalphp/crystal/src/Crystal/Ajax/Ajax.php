55b75d24b303f5e512aa4e32a522ba694ac61032
parsa mpsh <parsa84sh1384@gmail.com>
2020-02-03 20:22:38 +0330

    add group function to Ajax

--- vendor/crystalphp/crystal/src/Crystal/Ajax/Ajax.php
+++ vendor/crystalphp/crystal/src/Crystal/Ajax/Ajax.php
@@ -5,13 +5,20 @@ namespace Crystal\Ajax;
 class Ajax{
 
     protected static $events = [];
+    protected static $before_name = '';
 
     public static function make($name , \Closure $action){
-        static::$events[$name] = $action;
+        static::$events[static::$before_name.$name] = $action;
+    }
+
+    public static function group($name , \Closure $func){
+    	static::$before_name = $name . '.';
+    	$func();
+    	static::$before_name = '';
     }
 
     public static function run($name){
-        $func = static::$events[$name];
+        $func = static::$events[static::$before_name.$name];
         $h = new Handler;
         return \call_user_func_array($func , [$h , $h->data()])->__toString();
     }
