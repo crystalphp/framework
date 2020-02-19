e23ea817a61e866a9845d818e8f41c7ae7273b93
parsa mpsh <parsa84sh1384@gmail.com>
2020-02-19 19:45:17 +0330

    change the default table name in migration template to ClassName

--- src/Crystal/Console/make_templates/migration.php
+++ src/Crystal/Console/make_templates/migration.php
@@ -6,12 +6,12 @@ use Crystal\Database\Migrations\Migration;
 
 class ClassName extends Migration{
     public function up(){
-        $this->create('tbl' , '
+        $this->create('ClassName' , '
         	
 		');
     }
 
     public function down(){
-        $this->dropIfExist('tbl');
+        $this->dropIfExist('ClassName');
     }
 }
