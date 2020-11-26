324b27c5ae22aff5c6d70d555034bf872d86acbb
parsa mpsh <parsampsh@gmail.com>
2020-11-26 22:44:16 +0330

    console: update license

--- src/Crystal/Console/Cmd/license.php
+++ src/Crystal/Console/Cmd/license.php
@@ -6,7 +6,7 @@ class license{
     public function handle($args){
         return 'MIT License
 
-Copyright (c) 2020 parsa mpsh <parsa84sh1384@gmail.com> as <crystalphp.com>
+Copyright (c) 2020 parsa mpsh <parsampsh@gmail.com>
         
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
