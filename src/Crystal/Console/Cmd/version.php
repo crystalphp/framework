abf08b39fc380739640195dce3d40426a96132e1
parsa mpsh <parsampsh@gmail.com>
2020-11-26 22:43:10 +0330

    console: update version

--- src/Crystal/Console/Cmd/version.php
+++ src/Crystal/Console/Cmd/version.php
@@ -4,10 +4,7 @@ namespace Crystal\Console\Cmd;
 
 class version{
 	public function handle($args){
-        $composer_json_contents = cfile()->read(app_path('/composer.json'));
-        $composer_json_contents = json_decode($composer_json_contents);
-        $version = $composer_json_contents->require->{"crystalphp/framework"};
-        $version = str_replace('^' , '' , $version);
-        return "You project crystal framework version is {$version}";
+		$version = 'v0.1';
+	        return "You project crystal framework version is {$version}";
 	}
 }
